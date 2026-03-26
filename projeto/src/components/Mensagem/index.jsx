cabcb5bab0c8356215acc0449cfef1c9459cf264
felipe <bgame4495@gmail.com>
2026-03-26 16:54:29 -0300

    salvar

---
+++ projeto/src/components/Mensagem/index.jsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function mensagem() {
+    return (
+        // comandos html
+        <div className='mensagem'>
+            <h1>componentes</h1>
+            <p>estes e um componente com uma mensagem simples</p>
+        </div>
+    )
+    
+}
+
+export default mensagem
